d17bcbeef594f1275df5cd51a5727a13ce15e1b3
Ryan Page <rpage326@gmail.com>
2013-11-07 00:32:08 -0500

    problem running ruby seed.rb. hittin the hay.

--- db/migrate/20131107040402_create_posts.rb
+++ db/migrate/20131107040402_create_posts.rb
@@ -1,7 +1,17 @@
 class CreatePosts < ActiveRecord::Migration
   def up
+     create_table :posts do |t|
+      t.string :title
+      t.string :link
+      t.text :body
+      t.integer :up_votes, default: 0
+      t.integer :down_votes, default: 0
+      t.integer :total_votes, default: 0
+      t.timestamps
+    end
   end
 
   def down
+    drop_table :posts
   end
 end
